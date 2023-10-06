103196acd7134a1a7da97fc9af6d2b75889c95f3
Sima M Zadeh <77807196+simamzadeh@users.noreply.github.com>
2023-10-06 14:52:55 +0100

    Initial commit creating file with challenge description

---
+++ packages_into_lockers.py
@@ -0,0 +1,8 @@
+# Challenge: Packages into Lockers!
+# Difficulty: Intermediate
+#
+# Amazon is developing a new locker system for Amazon deliveries to ensure customer packages are delivered in a secure location.
+# In order to assist delivery drivers, Amazon will develop software to be installed that will instruct which locker the driver should place the package in depending on its size.
+#
+# For the purposes of this question, design and implement the component of this system that will take a package's size and return the locker number the package should be stored in so that the customer can be notified where their package is located.
+# This solution should be able to simulate adding a package to the locker and removing a package given a locker number.
